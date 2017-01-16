6086812de5c5f8ebfb750f557f896f2fb0d6326f
Alessio Caciagli <ac2014@cam.ac.uk>
2017-01-16 00:15:55 +0000

    Added BrownianMotion2DOT file

---
+++ BrownianMotion2DOT.m
@@ -0,0 +1,39 @@
+close all; clc;
+
+%Parameter declaration
+
+dt = 1e-3; %Time step
+R = 1e-6; %Radius of bead
+eta = 0.001; %Viscosity
+T = 300; %Temperature
+k = [1e-6 1e-6]; %Trap elastic constant
+r_eq = [0 0]; %Trap Equilibrium position
+N = 1e+5; %Number of steps
+
+r0 = [0 0]; %Initial position
+
+% Pre-calculation coefficients
+kB=1.38e-23; %Boltzmann constant
+kBT=kB*T;
+D = kBT/(6*pi*eta*R);
+gamma = kBT/D;
+C1 = 1 - k/gamma*dt;
+C2 = k.*r_eq/gamma*dt;
+h = randn(N,2); % noise
+hs = sqrt(2*D*dt)*h; % scaled noise
+
+% Inizialization
+r = zeros(N,2);
+r(1,:) = r0;
+t = (0:1:N-1)'*dt;
+
+
+% Simulation
+% It simulates a 
+for n = 2:1:N
+    r(n,:) = C1.*r(n-1,:) + C2 + hs(n,:);
+    if mod(n,10000)==0
+        disp(n)
+    end
+end
+
